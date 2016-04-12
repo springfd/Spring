4768443e8e81eefb7e768ecc5633af4564618e27
shihweiyao <shihweiyao@gmail.com>
2016-04-12 20:15:22 +0800

    2016/4/12

---
+++ app/controllers/trade_controller.rb
@@ -0,0 +1,35 @@
+class TradeController < ApplicationController
+    before_filter :authenticate_user!
+    skip_before_action :verify_authenticity_token, only: [:post_back]
+  def index
+    require 'rest-client'
+    params = 
+        '{
+        "sender":"rest",
+        "ver":"1.0.0",
+        "mid":"999812666555044",
+        "tid":"T0000000",
+        "pay_type":1,
+        "tx_type":1,
+        "params":
+        {
+        "layout":"1",
+        "order_no":"NO01234562",
+        "amt":"120000",
+        "cur":"NTD",
+        "order_desc":"測試 3C 網站購物",
+        "capt_flag":"0",
+        "result_flag":"1",
+        "post_back_url":"http://140.113.151.76:10111/trade/post_back",
+        "result_url":"https://140.113.151.76:10111/trade/result"
+        }}'
+    res = RestClient.post("https://tspg-t.taishinbank.com.tw/tspgapi/restapi/auth.ashx" , params, :headers => {:content_type => 'json'})
+    redirect_to JSON.parse(res)["params"]["hpp_url"]
+  end
+  def post_back
+    logger.info "xxxxxxxxxxxxxxxxxxxxxxxx"
+  end
+  def result
+    logger.info "xxxxxxxxxxxxxxxxxxxxxxxx"
+  end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+  before_filter :authenticate_user!
+  def show
+  
+  end
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      sign_in(@user == current_user ? @user : current_user, :bypass => true)
+      flash[:notice] ='成功更改個人資料'
+      redirect_to controller: 'users', action: 'edit', id: current_user
+    else
+      render 'edit'
+    end
+  end
+  
+  private
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    #params[:user][:phone_no] = params[:phone_no_full]
+    #accessible = [ :first_name, :last_name, :avatar, :phone_no, :email, 
+    #               addresses_attributes:[:id, :postal, :county, :district, :address]] 
+    #params.require(:user).permit(accessible)
+  end
+end
