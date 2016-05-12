c5fa73c2f26fdddf06625796ba8d635525b67e1d
shihweiyao <shihweiyao@gmail.com>
2016-05-12 13:33:47 +0800

    update

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,12 +1,12 @@
 class Project < ActiveRecord::Base
    has_attached_file :pj_attachment,
-      :url  => "/assets/pj_attachments/:id/:style/:basename.:extension",
-      :path => ":rails_root/public/assets/pj_attachments/:id/:style/:basename.:extension"
+      :url  => "/assets/pj_attachments/:id/:style/:basename.:extension"
+      #:path => ":rails_root/public/assets/pj_attachments/:id/:style/:basename.:extension"
    do_not_validate_attachment_file_type :pj_attachment
    
    has_attached_file :pj_cover,
-      :url  => "/assets/pj_covers/:id/:style/:basename.:extension",
-      :path => ":rails_root/public/assets/pj_covers/:id/:style/:basename.:extension"
+      :url  => "/assets/pj_covers/:id/:style/:basename.:extension"
+      #:path => ":rails_root/public/assets/pj_covers/:id/:style/:basename.:extension"
    do_not_validate_attachment_file_type :pj_cover
    
    validates :name, :kind, :year, :donate_begin_at, :donate_end_at, :exe_desc,  presence: true
