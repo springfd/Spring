0422ecccf03ec1dbe50bfeb21afbb3e08325b779
shihweiyao <shihweiyao@gmail.com>
2016-06-01 21:51:58 +0800

    update

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,7 @@
 class Project < ActiveRecord::Base
    has_many :donations, :dependent => :destroy
    has_many :stages, :dependent => :destroy
-   has_attached_file :pj_attachment, :styles => { :original => "1024>" },
+   has_attached_file :pj_attachment,
       :url  => "/assets/pj_attachments/:id/:style/:basename.:extension"
       #:path => ":rails_root/public/assets/pj_attachments/:id/:style/:basename.:extension"
    do_not_validate_attachment_file_type :pj_attachment
