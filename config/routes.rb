d6b707241889fba5bd2403235cc465b5f95708de
shihweiyao <shihweiyao@gmail.com>
2016-06-06 21:33:41 +0800

    2016/6/6

--- config/routes.rb
+++ config/routes.rb
@@ -22,45 +22,18 @@ Rails.application.routes.draw do
     post 'main/donation'
     
     
-    
     get  'donation', to: 'main#donation'
-    
+  
     get  'main/about'
+    get  'main/boardMembers'
+    get  'main/meetings' 
+    get  'main/contactUs' 
+    get  'main/otherLinks'        
     get  'main/news'
     get  'main/donation' 
     get  'main/downloadManual' 
     root 'main#about'
-  end
-<<<<<<< HEAD
-=======
-  devise_for :users, :controllers => { :registrations => "registrations", :confirmations => "users/confirmations" } # be careful of the order with resources users
-  resources :users, only: [:index, :show, :destroy]
-  
-  post 'projects/checkLogin'
-  post 'projects/delete_stage'
-  post 'projects/ajax_update'
-  
-  get  'main/post_back'
-  post 'main/result'
-  post 'main/post_back'
-  post 'main/donation'
-  
-  
-  
-  get  'donation', to: 'main#donation'
-  
-  get  'main/about'
-  get  'main/boardMembers'
-  get  'main/meetings' 
-  get  'main/contactUs' 
-  get  'main/otherLinks'        
-  get  'main/news'
-  get  'main/donation' 
-  get  'main/downloadManual' 
-  root 'main#about'
   
   end
-
   
->>>>>>> b12117a543b26b9930e8aa889ca561d1a456c560
 end
